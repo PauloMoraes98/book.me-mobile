719f5ceaf980b6ce7ca56aa9a4f96e7b34560e7b
Paulo Cesar <paulo.csm_@hotmail.com>
2020-07-13 23:26:31 -0300

    initial commit

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import BookDetails from '../screens/BookDetails/index';
+import Login from '../screens/Login/index';
+import MainScreen from '../screens/MainScreen/index';
+import NewPassword from '../screens/NewPassword/index';
+import Profile from '../screens/Profile/index';
+import Register from '../screens/Register/index';
+
+const AppStack = createStackNavigator();
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator screenOptions={{ headerShown: false }}>
+                <AppStack.Screen name="BookDetails" component={ BookDetails } />
+                <AppStack.Screen name="Login" component={ Login } />
+                <AppStack.Screen name="MainScreen" component={ MainScreen } />
+                <AppStack.Screen name="NewPassword" component={ NewPassword } />
+                <AppStack.Screen name="Profile" component={ Profile } />
+                <AppStack.Screen name="Register" component={ Register } />
+            </AppStack.Navigator>
+        </NavigationContainer>
+    );
+}
